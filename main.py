cba878bc86d376c6a0244b58433e66b2b54b9d9e
Adam Burke <marius.bagnal@my.gavilan.edu>
2020-07-21 13:40:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import praw
+import discord
+import asyncio
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    global channel
+    print('Logged in as {0.user}'.format(client))        
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit('copypasta')
+def refresh():
+    for submission in subreddit.new(limit=1):
+        var = submission.selftext
+
+
+@client.event
+async def background_loop():
+    await client.wait_until_ready()
+    while not client.is_closed():
+        channel = client.get_channel(735108850809569311)
+        refresh()
+        print('sending message')
+        print(channel)
+        await asyncio.sleep(2)
+        await channel.send(var)
+        print('sent: ' + var)
+        await asyncio.sleep(5)
+
+
+client.loop.create_task(background_loop())
+
+client.run('NzM1MDk5MTE2MDE0NjY1NzYx.XxbUcg.wt3fW6gjnwId5CAtmho_bKLQg1k')
