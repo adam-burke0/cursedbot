104dcbe112a334edfd8234fe9eb6f088d62d0c7d
Adam Burke <marius.bagnal@my.gavilan.edu>
2020-07-24 12:59:08 +0100

    Cleaned up code and shit

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import praw
 import discord
 import asyncio
 client = discord.Client()
+BOT_TOKEN = ''
 
-@client.event
-async def on_ready():
-    global channel
-    print('Logged in as {0.user}'.format(client))        
-reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit('copypasta')
+# Sets up reddit bot and chooses subreddit
+
+def setup():
+    global reddit
+    global subreddit
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit('copypasta')
+
+# Gets latest submission from r/copypasta and saves the content to a variable
+
 def refresh():
     global var
     for submission in subreddit.new(limit=1):
         var = submission.selftext
+
+# Prints to console when bot connects
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))        
+
+# Sends copypasta to #copypasta chat on Vibestation every 20 minutes
 
 @client.event
 async def background_loop():
@@ -22,9 +35,12 @@ async def background_loop():
     while not client.is_closed():
         global var
         refresh()
+        
+        #If copypasta is longer than Discord's 2000 character limit then it will wait 10 minutes and refresh
         if len(var) > 1500:
+            await asyncio.sleep(600)
             refresh()
-            600
+        #If copypasta is shorter than 1500 characters then the copypasta gets sent and the timer resets to 20 minutes before retrying    
         elif len(var) < 1500:
             channel = client.get_channel(735108850809569311)
             print('sending message in channel: ' + str(channel))
@@ -32,7 +48,8 @@ async def background_loop():
             print('sent: ' + var)
             await asyncio.sleep(1200)
 
-
+# Sets the background_loop function go run in the background
 client.loop.create_task(background_loop())
 
-client.run('NzM1MDk5MTE2MDE0NjY1NzYx.XxbUcg.wt3fW6gjnwId5CAtmho_bKLQg1k')
+# Brings the bot online and begins the loop
+client.run(BOT_TOKEN)
